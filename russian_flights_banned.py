a60b7c32d3d51c2645470629b8ef73b380833483
azotamiota <azotamiota@gmail.com>
2022-02-28 21:21:58 +0100

    code cleaned

--- russian_flights_banned.py
+++ russian_flights_banned.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.patheffects as PathEffects
 from matplotlib.font_manager import FontProperties
 
-
 # For testing purposes in PyCharm
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -23,28 +22,9 @@ def draw_plot():
 
     # Import data of min wages 2016-2021
     df = pd.read_csv('russian_flights_banned.csv', encoding='latin1')
-    print('this is df: \n', df)
-    #
-    # # Clean the data
-    # df.drop(['Flag and Footnotes'], axis=1, inplace=True)
-    # df.drop(df[df['banned'] == 'no'].index, inplace=True)
-    # df.replace(to_replace="Germany (until 1990 former territory of the FRG)", value="Germany", inplace=True)
-    # df.replace(to_replace=':', value='NaN', inplace=True)
-    # df.sort_values('GEO', inplace=True)
-    # df.rename(columns={'GEO':'name'}, inplace=True)
-    # df['Value']=df['Value'].astype(float)
-    # df['Value']=df['Value'].round()
-    # df.dropna(axis=0, how='any', inplace=True)
-    # df['Value']=df['Value'].apply(lambda x: int(x))
-    #
-    # # Show only second half of 2020
-    # df.drop(df[df['TIME'] != '2020S2'].index, inplace=True)
 
     # Load base map
     world = gpd.read_file('ne_10m_admin_0_countries')
-    print('world geodb: \n', world)
-    # Customize column names for merging with DataFrame
-    world.rename(columns={'name':'country_name'}, inplace=True)
 
     # Set better projection
     world = world.to_crs(epsg=3035)
@@ -57,39 +37,24 @@ def draw_plot():
 
     # Merge the map and data
     table = world.merge(df, right_on='country', left_on='NAME')
-    # print(table)
+
     # Import data that shows coordinates of centres of all countries
     countrycentre=pd.read_csv('countriescentre.csv', encoding='latin1')
-    #print('countryCentre file: \n',countrycentre)
 
-    # # Customize for merging
-    # countrycentre.replace(to_replace='Czech Republic', value='Czechia', inplace=True)
-
-    # Merge DataFrames to show minimum wages and coordinates of countries in Europe
+    # Merge DataFrames to show ban info of countries in Europe
     centre_with_banned_info = df.merge(countrycentre)
-    #print('centre with banned info \n', centre_with_banned_info)
-    #
+
     # Create geopandas DataFrame
     centre_with_banned_info_gdf = gpd.GeoDataFrame(centre_with_banned_info, crs=4326,
                         geometry=gpd.points_from_xy(x=centre_with_banned_info.longitude,
                                                      y=centre_with_banned_info.latitude))
     print('centre with banned info gdb: \n', centre_with_banned_info_gdf)
+
     # Suit projection to base map
     centre_with_banned_info_gdf = centre_with_banned_info_gdf.to_crs(epsg=3035)
 
-    # Optional markers on map indicates countries:
-    centre_with_banned_info_gdf.plot(marker='.', markersize=3 ,color='red', ax=base)
-
     # Plot ready map
-    table.plot(ax=base, edgecolor='black', linewidth=.4, legend=True, column='status', cmap='Spectral', #vmax=2000, vmin= 300,
-              # Customize appearance of legend
-
-                legend_kwds={'fontsize': 15}
-
-              #  legend_kwds={'location': 'bottom', 'spacing': 'proportional',
-              #               'shrink': .50, 'pad': -0.09,
-              #               'aspect': 50}
-               )
+    table.plot(ax=base, edgecolor='black', linewidth=.4, legend=True, column='status', cmap='Spectral')
 
     fp = FontProperties(fname=r"Font Awesome 6 Free-Solid-900.otf")
 
@@ -97,10 +62,9 @@ def draw_plot():
     for idx, row in centre_with_banned_info_gdf.iterrows():
         if row['status'] == 'banned':
             txt = plt.text(x=row.geometry.x, y=row.geometry.y, s="\ue069", fontproperties=fp,
-                            color='white', # fontsize=(row['Value'] ** 0.35),
-                           family='arial', # rotation=row['longitude']/4,
-                           wrap=True, # fontproperties={'weight':1000, 'variant':'small-caps'},
-                          horizontalalignment='center', verticalalignment='center')
+                            color='white', family='arial', wrap=True,
+                           horizontalalignment='center', verticalalignment='center')
+
         # Set black border color of each characters for better readability
             txt.set_path_effects([PathEffects.withStroke(linewidth=2, foreground='black')])
 
@@ -114,7 +78,7 @@ def draw_plot():
     # Remove axes
     plt.xticks([])
     plt.yticks([])
-    #
+    
     # Zoom the area of Europe
     bounds = centre_with_banned_info_gdf.geometry.bounds
     plt.xlim([bounds.minx.min()-0.5*1e6, bounds.maxx.max()+0.3*1e6])
